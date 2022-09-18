0ebe3013a609c8179384f4958969d44810760c17
Victor Freitas <tqi_valves@uolinc.com>
2022-09-18 12:49:08 -0300

    Initial Commit

---
+++ TQIHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TQIHub
+//
+//  Created by Victor Alves de Freitas on 18/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TQIHubUITests/TQIHubUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TQIHubUITestsLaunchTests.swift
+//  TQIHubUITests
+//
+//  Created by Victor Alves de Freitas on 18/09/22.
+//
+
+import XCTest
+
+class TQIHubUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
